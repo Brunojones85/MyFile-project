47f65062460cd4000509b0d6ca298b5b25f76897
João Vitor <jv.carvalho@gmail.com>
2017-07-12 11:06:03 -0300

    upload funcionando na pagina /uploads

--- MyFile/www/js/controllers.js
+++ MyFile/www/js/controllers.js
@@ -29,6 +29,26 @@ angular.module('starter.controllers', [])
   });
 })
 
+.controller('UploadController', function($scope, $http) {
+   
+    $scope.enviar = function(){
+      var formData = new FormData();
+      var arquivo = document.getElementById("arquivoInput").files[0];
+      formData.append("file", arquivo);
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          var div = document.getElementById('mensagem');
+          var resposta = xhr.responseText;
+          div.innerHTML += resposta;
+          }
+        }
+        xhr.open("POST", "http://localhost:3000/upload");
+        xhr.send(formData);
+      }
+  })
+
+
 .controller('ArquivosController', function($scope, $http,) {
     $http.get('http://localhost:3000/arquivo').then(function(reposta){
     $scope.arquivos = reposta.data;
